abb9beaecc3bb99cc0c147d916a5edbe31dff933
STS <1078998107@qq.com>
2016-12-20 20:09:10 +0800

    首页基本布局完成 Signed-off-by: STS <1078998107@qq.com>

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,10 +13,22 @@ angular.module('surveyTimeApp', ["ui.router"])
 	
 	$stateProvider.state("home",{
 			url:"/home",
-      templateUrl:'views/home.html'
+      templateUrl:'views/home.html',
+      controller:"stsCon"
     }).state("home.news",{
 			url:"/news",
-      templateUrl:'views/news.html'
+      templateUrl:'views/news.html',
+      controller:"news"
+    }).state("home.topic",{
+			url:"/topic",
+      templateUrl:'views/topic.html',
+      controller:"topic"
+    }).state("home.xt",{
+			url:"/xt",
+      templateUrl:'views/xt.html'
+    }).state("home.xt.dx",{
+			url:"/dx",
+      templateUrl:'views/dx.html'
     })
 
 	$urlRouterProvider.when("","/home");
--- app/scripts/controllers/sts.js
+++ app/scripts/controllers/sts.js
@@ -8,6 +8,14 @@
  * Controller of the surveyTimeApp
  */
 angular.module('surveyTimeApp')
-	.controller('stsCon',['$scope', function ($scope) {
-    
+	.controller('stsCon',['$scope',"$rootScope", function ($scope) {
+		
+  }])
+	.controller('news',['$scope',"$rootScope",function ($scope,$rootScope) {
+		$scope.aa = function(){
+			$rootScope.title = $scope.title;
+		}
+  }])
+	.controller('topic',['$scope',"$rootScope",function ($scope,$rootScope) {
+    	
   }]);
