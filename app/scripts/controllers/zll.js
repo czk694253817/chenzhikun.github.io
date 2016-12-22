24a296db116f7b36e86e7200fa6f8c61b080d849
zhulinlin <zhulinlin@126.com>
2016-12-22 15:48:29 +0800

    zhulinlinshanchu1
    Signed-off-by: zhulinlin <zhulinlin@126.com>

--- app/scripts/controllers/zll.js
+++ app/scripts/controllers/zll.js
@@ -28,16 +28,7 @@ angular.module('surveyTimeApp')
 					$scope.zhu=true;
 				}
 				$scope.data=e.data;
-				//删除
-				$scope.sc=function(hj){
-				console.log(hj)
-				e.data.splice(hj,1)
-			/*	delete $scope.data[hj] */
-				$scope.data=e.data;
-				console.log(e)
-			return e;
-			}
-				/*console.log($scope.data.length/6)*/
+	
 			},function(){/*$state.go('404')*/})
 			$scope.fn2=function(){
 		    if($scope.data.length/6<=$scope.shu+1){
@@ -45,9 +36,18 @@ angular.module('surveyTimeApp')
 		        }else{
 		        	$scope.shu++
 		        }
-				
-		
-					
+			}
+				//删除
+				$scope.sc=function(e,hj){
+					 $http({
+       url:url+'item/'+'<e.id>',
+       method:"delete"
+     }).then(function(){
+       
+       $scope.data.splice(hj,1)
+     },function(){})
+				console.log(e.id)
+	
 		
 			}
 			$scope.kk=function(hh){
@@ -71,7 +71,7 @@ angular.module('surveyTimeApp')
 	}]).directive('shuju',function(){//自定义指令
     return {
         restrict: 'EACM',//仅限元素名调用
-        template: '<div><li class="list-group-item"  ng-click="fn()"><span class="badge">0</span>{{x.title}}</li><div class="z-d" style="display:none;"><div class="zk-p" ng-click="kk(x.id)"><img src="images/z-zl.png"/><span>分享</span></div><div class="zk-p"><img src="images/z-sds.png"/><span>结果</span></div><div class="zk-p" ng-click="sc($index)"><img src="images/z-dd.png"/><span>删除</span></div></div></div>',
+        template: '<div><li class="list-group-item"  ng-click="fn()"><span class="badge">0</span>{{x.title}}</li><div class="z-d" style="display:none;"><div class="zk-p" ng-click="kk(x.id)"><img src="images/z-zl.png"/><span>分享</span></div><div class="zk-p"><img src="images/z-sds.png"/><span>结果</span></div><div class="zk-p" ng-click="sc(x,$index)"><img src="images/z-dd.png"/><span>删除</span></div></div></div>',
      
   link:function(scope,ele,attr){
 //	  scope.po=false;
