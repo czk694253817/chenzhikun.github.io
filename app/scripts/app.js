be80c45fc0ad9a49ad469a0659bde7437e57fa4a
STS <1078998107@qq.com>
2016-12-23 18:58:19 +0800

    提交数据构思已完成
    Signed-off-by: STS <1078998107@qq.com>

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,47 +8,65 @@
  *
  * Main module of the application.
  */
-
-
 angular.module('surveyTimeApp', ["ui.router","chart.js"])
 .constant("url","http://47.90.20.200:1602/")
 .config(["$stateProvider","$urlRouterProvider",function($stateProvider,$urlRouterProvider){
 $urlRouterProvider.when("","/home")
-
-
-	$stateProvider.state("home",{
-			url:"/home",
+  $stateProvider.state("home",{
+    url:"/home",
       templateUrl:'views/home.html',
       controller:"stsCon"
     }).state("home.news",{
-			url:"/news",
+    url:"/news",
       templateUrl:'views/news.html',
       controller:"news"
     }).state("home.results",{
-			url:"/results",
+    url:"/results",
       templateUrl:'views/results.html',
       controller:"czkCon"
     }).state("home.topic",{
-			url:"/topic",
+    url:"/topic",
       templateUrl:'views/topic.html',
       controller:"topic"
     }).state("home.xt",{
-			url:"/xt",
+    url:"/xt",
       templateUrl:'views/xt.html',
       controller:"sxt"
     }).state("home.xt.dx",{
-			url:"/dx",
+    url:"/dx",
       templateUrl:'views/dx.html',
       controller:"dx"
     }).state("home.lists",{
-        url:"/lists",
-        templateUrl:"views/lists.html",
+      url:"/lists",
+      templateUrl:"views/lists.html",
       controller:"zllCon" 
+    }).state("zhuce",{
+      url:"/zhuce",
+      templateUrl:"views/zhuce.html",
+      controller:"zhuceCon"
+    }).state("login",{
+      url:"/login",
+      templateUrl:"views/login.html",
+      controller:"loginCon"
+    }).state("zhuyemian",{
+      url:"/zhuyemian",
+      templateUrl:"views/zhuyemian.html",
+      controller:"zhuyemianCon"
     }).state("404",{
-        url:"/404",
-        templateUrl:"404.html"
+      url:"/404",
+      templateUrl:"404.html"
+    }).state("home.xt.dxt",{
+      url:"/dxt",
+      templateUrl:'views/stsdxt.html'
+    }).state("home.xt.tk",{
+      url:"/tk",
+      templateUrl:'views/ststk.html',
+      controller:"dx"
+    }).state("home.xt.jd",{
+      url:"/jd",
+      templateUrl:'views/stsjd.html'
+    }).state("home.topic.ststotk",{
+      url:"/ststotk",
+      templateUrl:'views/ststotk.html'
     })
-
-
 }]);
-
