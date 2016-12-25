de113d9bfeb5b08ec7e1fb622a6edcbdebfd384e
STS <1078998107@qq.com>
2016-12-25 21:13:56 +0800

    点击删除问题下的选项
    Signed-off-by: STS <1078998107@qq.com>

--- app/scripts/controllers/sts.js
+++ app/scripts/controllers/sts.js
@@ -14,8 +14,10 @@ angular.module('surveyTimeApp')
 			$window.history.back();
 		}
 		$scope.s_type=0;
+		$scope.sts_sqq=true;
+		$scope.sts_sqqq=true;
 		// $rootScope.aa=$stateParams.id;
-		console.log($stateParams.id)
+		// console.log($stateParams.id)
 		
   }])
 	.controller('news',['$scope',"$rootScope","$state",function ($scope,$rootScope,$state) {
@@ -58,7 +60,11 @@ angular.module('surveyTimeApp')
     	$scope.sdx=function(){
     		$rootScope.sarr.push({"op":"选项名称"})
     	}
-
+    	/*删除*/
+    	$scope.sts_shch=function(index){
+    		$rootScope.arr.splice(index,1)
+    	}
+    	/*点击切换模板*/
     	$scope.xxk=function(index){
     		$scope.s_type=index
     		angular.element(".sts_type").eq(index).addClass("sts_active").siblings().removeClass("sts_active");
@@ -66,27 +72,42 @@ angular.module('surveyTimeApp')
     			$state.go("home.xt.dx")
 
     		}else if(index==1){
-    			$state.go("home.xt.dxt")
+    			$state.go("home.xt.dx")
     		}else if(index==2){
     			$state.go("home.xt.tk")
     		}else if(index==3){
     			$state.go("home.xt.jd")
     		}	
     	}
-
+    	/*点击确认*/
     	$scope.sque=function(){
-    		$scope.boss=[];
-    		console.log($scope.s_type)
-    		$rootScope.titl=$scope.titl
-    		console.log($rootScope.titl)
-    		for(var i=0;i<$scope.arr.length;i++){
-    			console.log($scope.arr[i].detail)
-    			$scope.boss.push({"op":$scope.arr[i].detail})
-    		}
-    		$scope.boss.push({"title":$scope.titl})
-    		$scope.boss.push({"type":$scope.s_type})
-    		console.log($scope.boss)
+    		if($scope.titl==undefined||$scope.titl==""){
+    			return $scope.sts_sqq=false;
+    		}else{
+    			$scope.sts_sqq=true;
+    			$scope.boss=[];
+    			$scope.opt=[];
+    			
+	    		for(var i=0;i<$scope.arr.length;i++){
+	    			console.log($scope.arr[i].detail)
+	    			if($scope.arr[i].detail==undefined||$scope.arr[i].detail==""){
+	    				return $scope.sts_sqqq=false;
+	    			}else{
+	    				$scope.sts_sqqq=true;
+	    				$scope.opt.push({"op":$scope.arr[i].detail,"num":0})
+	    				
+	    			}
+	    			
+	    		}
+	    		$scope.boss.push($scope.opt)
+	    		$scope.boss.push({"title":$scope.titl})
+	    		$scope.boss.push({"type":$scope.s_type})
+	    		console.log($scope.boss)
 
+    		}
+
+
+    		
     	}
 
   }])
