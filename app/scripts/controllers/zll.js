97b2ee1dded9738148f63e4015a022cec6d6e8db
zhulinlin <zhulinlin@126.com>
2016-12-28 20:24:32 +0800

    12/28zhulinlin
    Signed-off-by: zhulinlin <zhulinlin@126.com>

--- app/scripts/controllers/zll.js
+++ app/scripts/controllers/zll.js
@@ -21,10 +21,9 @@ angular.module('surveyTimeApp')
 			method: 'get',
 			url: url + 'item/',
 			params: {
-				uid: 'cdefcdc48ec159c5'
+				uid: $scope.id
 			}
 		}).then(function(e) {
-			console.log(e)
 			if(e.data == '' || e.data == 'null' || e.data.length == 0) {
 				console.log('ppp')
 				$scope.we = true;
@@ -40,7 +39,7 @@ angular.module('surveyTimeApp')
 				$scope.zhu = false;
 			}
 			$scope.data = e.data;
-			console.log($scope.data.length)
+			console.log($scope.data)
 		}, function() { /*$state.go('404')*/ })
 		$scope.fn2 = function() {
 			if($scope.data.length / 6 <= $scope.shu + 1) {
@@ -61,22 +60,34 @@ angular.module('surveyTimeApp')
 		}
 		$scope.zp = function() {
 				$state.go('home.news')
-			}
+		}
 			//删除
 		$scope.sc = function(e, hj) {
-			console.log(e);
 			$http({
 				url: url + 'item/' + e.id,
 				method: "delete"
 			}).then(function(cd) {
-				console.log(cd);
 				if(cd.data.count == '1') {
+							angular.element(".alertw").css('bottom', '0')
+				angular.element(".alertw").stop().animate({
+					"bottom": "18%",
+					"opacity": 1
+				}, 600, function() {
+			angular.element(".alertw").delay(1000).animate({
+						"opacity": 0
+					});
+				})
+				$scope.shj = '删除成功';
 		$scope.data.splice($scope.data.indexOf(e), 1);
+		$scope.data=$scope.data
 				}
 			}, function() {
 				console.log(hj);
 			})
 		}
+		$scope.$watch('data',function(){
+			/*$scope.data=$scope.data*/
+		})
 		$scope.kk = function(hh) {
 			$scope.ll = true;
 			angular.element(".z-kj").slideToggle(500)
@@ -126,14 +137,14 @@ angular.module('surveyTimeApp')
 	}).directive('shujw', function() { //自定义指令
 		return {
 			restrict: 'EACM', //仅限元素名调用
-			template: '<div class="z-kj"style="display: none;"> <div class="bshare-custom icon-medium-plus" title="C#面向对象程序设计" url="http://blog.csdn.net/pan_junbiao/article/details/5308139" pic="http://i0.sinaimg.cn/ty/g/pl/2014-01-05/U9977P6T12D6966211F1286DT20140105031220.jpg"><span class="sz-p">分享到:</span><br /><div class="lk"><a title="分享到QQ空间" class="bshare-qzone"></a><span>QQ空间</span></div><div class="lk"><a title="分享到新浪微博" class="bshare-sinaminiblog"></a><span>新浪微博</span></div><div class="lk"><a title="分享到微信" class="bshare-weixin" href="javascript:void(0);"></a><span>微信好友</span></div><div class="lk"><a title="分享到腾讯微博" class="bshare-qqmb"></a><span>腾讯微博</span></div><div class="lk"><a title="分享到网易微博" class="bshare-neteasemb"></a><span>网易微博</span></div><div class="lk"><a title="分享到QQ好友" class="bshare-qqim" href="javascript:void(0);"></a><span>QQ好友</span></div></div></div>',
+			template: '<div class="z-kj"style="display: none;"> <div class="bshare-custom icon-medium-plus" title="C#面向对象程序设计" url="http://blog.csdn.net/pan_junbiao/article/details/5308139" pic="http://i0.sinaimg.cn/ty/g/pl/2014-01-05/U9977P6T12D6966211F1286DT20140105031220.jpg"><span class="sz-p">分享到:</span><br /><div class="bsPromo bsPromo1"></div><div class="lk"><a title="分享到QQ空间" class="bshare-qzone"></a><span>QQ空间</span></div><div class="lk"><a title="分享到新浪微博" class="bshare-sinaminiblog"></a><span>QQ空间</span></div><div class="lk"><a title="分享到微信" class="bshare-weixin" href="javascript:void(0);"></a><span>QQ空间</span></div><div class="lk"><a title="分享到腾讯微博" class="bshare-qqmb"></a><span>QQ空间</span></div><div class="lk"><a title="分享到网易微博" class="bshare-neteasemb"></a><span>QQ空间</span></div><div class="lk"><a title="分享到QQ好友" class="bshare-qqim" href="javascript:void(0);"></a><span>QQ空间</span></div></div>',
 			link: function(scope, ele, attr) {
-	ele.find(".lk").children('a').click(function() {
-		console.log('pppp')
+	ele.find(".icon-medium-plus").find('a').click(function() {
+		console.log($(this).parent().parent().attr("url"))
 	var parents = $(this).parent().parent();
 	bShare.addEntry({
     title: parents.attr("title"),
-	url: parents.attr("url"),
+    url: parents.attr("url"),
 	pic: parents.attr("pic")
    });
     });
